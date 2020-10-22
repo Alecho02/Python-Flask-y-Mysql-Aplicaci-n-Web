12c7dd68dbc673a6cab3da2a60deae39585d5ced
Alecho02 <alexandermagno.pacheco@gmail.com>
2020-10-21 18:14:01 -0600

    update2

--- App.py
+++ App.py
@@ -10,6 +10,14 @@ def Index():
 def add_contact():
 	return 'Agregar contacto'
 
+@app.route('/edit')
+def edit_contact():
+	return 'Editar contacto'
+
+@app.route('/add_contact')
+def add_contact():
+	return 'Agregar contacto'
+
 
 if __name__ == '__main__':
 	app.run(port = 3000, debug = True) 
