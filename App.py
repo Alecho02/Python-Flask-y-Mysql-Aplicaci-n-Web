0e7258fe5cd5b5fb50973458cf0e8a7d64fbb49f
Alecho02 <alexandermagno.pacheco@gmail.com>
2020-10-21 18:10:21 -0600

    update2

--- App.py
+++ App.py
@@ -2,5 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def Index():
+	return 'Hola Mundo'
+
+@app.route('/add_contact')
+def add_contact():
+	return 'Agregar contacto'
+
+
 if __name__ == '__main__':
-	app.run(port = 3000, debug = True)
+	app.run(port = 3000, debug = True) 
