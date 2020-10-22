e6cf7acf64618617025aa28e212d613c22f835cf
Alecho02 <alexandermagno.pacheco@gmail.com>
2020-10-21 22:01:51 -0600

    update3

--- App.py
+++ App.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_mysqldb import MyQSL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] ='localhost'
+app.config['MYSQL_USER'] ='admin'
+app.config['MYSQL_PASSWORD'] ='Bancosal1'
+app.config['MYSQL_DB'] ='flaskcontacts'
+mysql=MyQSL(app)
 
 @app.route('/')
 def Index():
-	return 'Hola Mundo'
+	return ''
 
 @app.route('/add_contact')
 def add_contact():
@@ -14,9 +20,9 @@ def add_contact():
 def edit_contact():
 	return 'Editar contacto'
 
-@app.route('/add_contact')
-def add_contact():
-	return 'Agregar contacto'
+@app.route('/delete')
+def delete_contact():
+	return 'Borrar contacto'
 
 
 if __name__ == '__main__':
